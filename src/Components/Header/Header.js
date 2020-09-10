ba84f2bc9dd0ff3ec7888cefd342cfc5ec7fb1b8
User <user@users-imac.home>
2020-09-10 07:45:50 -0400

    updating the app

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = props => (
+    <React.Fragment>
+        <header className="header">some text...</header>
+    </React.Fragment>
+);
+
+export default Header;
