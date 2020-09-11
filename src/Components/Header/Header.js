7ab35ad85750617698c6f0c719008338f4125b26
User <user@users-imac.home>
2020-09-11 10:17:18 -0400

    updating the header button; will need to make this all separate components maybe

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,12 +9,15 @@ const Header = props => (
             <div className="logo-box">
                 <img src={logo} alt="Logo" className="logo"></img>
             </div>
+            <div className="fake-box"></div>
 
             <div className="text-box">
                 <h1 className="heading-primary">
                     <span className="heading-primary-main">Outdoors</span>
                     <span className="heading-primary-sub">is where life happens</span>
                 </h1>
+
+                <a href="#" className="btn btn-white">Discover our tours</a>
             </div>
 
         </header>
