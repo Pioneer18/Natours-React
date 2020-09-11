166da917e38d0205125cff95b0ec4d23e596a533
User <user@users-imac.home>
2020-09-11 11:52:11 -0400

    updated the header to be in components

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css'
+
+const Button = props => (
+    <a href="#" className="btn btn-white btn-animated">Discover our tours</a>
+);
+
+export default Button;
