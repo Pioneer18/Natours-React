e88906aefac5e339d9e709580d210efa8fa97f6d
User <user@users-imac.home>
2020-09-10 22:22:30 -0400

    updated the header of the application

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../Images/logo-white.png'
 
 const Header = props => (
     <React.Fragment>
-        <header className="header">Some text...</header>
+        <header className="header">
+            {/*  An image should have a block level container, which can be more easily placed where it needs to be */}
+            <div className="logo-box">
+                <img src={logo} alt="Logo" className="logo"></img>
+            </div>
+
+            <div className="text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary-main">Outdoors</span>
+                    <span className="heading-primary-sub">is where life happens</span>
+                </h1>
+            </div>
+
+        </header>
     </React.Fragment>
 );
 
