f142e7d357718a320b5234f7663e5e15c69ec788
User <user@users-imac.home>
2020-09-17 14:01:22 -0400

    updated the main section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Main></Main>
+      <Main headingMargin={'8rem'}></Main>
     </div>
   );
 }
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -9,9 +9,24 @@ const Main = props => (
     < >
         <main>
             <section className="section-about">
-                <h2 className="heading-secondary">
-                    Exciting tours for adventurous people
-                </h2>
+                {/** Header */}
+                <div className="u-center-text" style={{marginBottom: props.headingMargin}}>
+                    <h2 className="heading-secondary">
+                        Exciting tours for adventurous people
+                    </h2>
+                </div>
+                {/** Row (props: text for column 1, srcs for images in column 2) */}
+                <div className="row">
+                    <div className="col-1-of-2">
+                        <h3 className="heading-tertiary">You're going to fall in love with nature</h3>
+                        <p className="paragraph">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nostrum quidem dicta eaque repudiandae consectetur totam nobis dolorum, exercitationem nisi optio dignissimos assumenda facere cum nam autem magni mollitia repellendus.
+                        </p>
+                    </div>
+                    <div className="col-1-of-2">
+                        Image composition
+                    </div>
+                </div>
             </section>
         </main>
     </>
