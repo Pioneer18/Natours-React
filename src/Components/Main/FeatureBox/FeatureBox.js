df243aa5e2d461d1119f0617cfed54b7ee03395d
User <user@users-imac.home>
2020-09-17 18:22:42 -0400

    updating the feature box styling

--- src/Components/Main/FeatureBox/FeatureBox.js
+++ src/Components/Main/FeatureBox/FeatureBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../../../sass/Components/_feature-box.scss'
 
 const FeatureBox = props => (
     <>
         <div className="feature-box">
             {/** Linea Icon Font: <i /> is for icon font*/}
             <i className="feature-box__icon icon-basic-world"></i>
-            <h3 className="heading-tertiary">Explore the world</h3>
+            <h3 className="heading-tertiary u-margin-bottom-small">Explore the world</h3>
             <p className="feature-box__text">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, molestias iure tempore repellat porro cupiditate.
             </p>
