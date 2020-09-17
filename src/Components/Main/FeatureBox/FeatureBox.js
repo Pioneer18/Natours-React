c39d9e7ebb3c275b1fbca0f1c0bc057da48f1b48
User <user@users-imac.home>
2020-09-17 19:19:52 -0400

    setup the feature block and added 3 more to the sectionfeatures component

--- src/Components/Main/FeatureBox/FeatureBox.js
+++ src/Components/Main/FeatureBox/FeatureBox.js
@@ -5,10 +5,10 @@ const FeatureBox = props => (
     <>
         <div className="feature-box">
             {/** Linea Icon Font: <i /> is for icon font*/}
-            <i className="feature-box__icon icon-basic-world"></i>
-            <h3 className="heading-tertiary u-margin-bottom-small">Explore the world</h3>
+            <i className={`feature-box__icon ${props.icon}`}></i>
+            <h3 className="heading-tertiary u-margin-bottom-small">{props.header}</h3>
             <p className="feature-box__text">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, molestias iure tempore repellat porro cupiditate.
+                {props.text}
             </p>
         </div>
     </>
--- src/Components/Main/SectionFeatures/SectionFeatures.js
+++ src/Components/Main/SectionFeatures/SectionFeatures.js
@@ -7,7 +7,32 @@ const SectionFeatures = props => (
     <section className="section-features">
         <div className="row">
             <div className="col-1-of-4">
-                <FeatureBox></FeatureBox>
+                <FeatureBox 
+                    icon={'icon-basic-world'}
+                    header={'Explore the World'}
+                    text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, molestias iure tempore repellat porro cupiditate.'}>
+                </FeatureBox>
+            </div>
+            <div className="col-1-of-4">
+                <FeatureBox 
+                    icon={'icon-basic-compass'}
+                    header={'meet nature'}
+                    text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, molestias iure tempore repellat porro cupiditate.'}>
+                </FeatureBox>
+            </div>
+            <div className="col-1-of-4">
+                <FeatureBox 
+                    icon={'icon-basic-map'}
+                    header={'fiind your way'}
+                    text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, molestias iure tempore repellat porro cupiditate.'}>
+                </FeatureBox>
+            </div>
+            <div className="col-1-of-4">
+                <FeatureBox 
+                    icon={'icon-basic-heart'}
+                    header={'Live a healthier life'}
+                    text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, molestias iure tempore repellat porro cupiditate.'}>
+                </FeatureBox>
             </div>
         </div>
     </section>
