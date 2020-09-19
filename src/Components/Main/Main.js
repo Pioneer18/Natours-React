2b6aa276cb0ad29136597a968433e27f493559c2
User <user@users-imac.home>
2020-09-18 20:07:11 -0400

    making a Tours Section component; SectionTours

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -6,12 +6,14 @@ import React from 'react';
 import '../../sass/layout/_main.scss';
 import SectionComposition from './SectionComposition/SectionComposition';
 import SectionFeatures from './SectionFeatures/SectionFeatures';
+import SectionTours from './SectionTours/SectionTours';
 
 const Main = props => (
     < >
         <main>
             <SectionComposition headingMargin={props.headingMargin}></SectionComposition>
             <SectionFeatures></SectionFeatures>
+            <SectionTours></SectionTours>
         </main>
     </>
 );
---
+++ src/Components/Main/SectionTours/SectionTours.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SectionTours = props => (
+    <  >
+     {/** Header */}
+        <div className="u-center-text" style={{ marginBottom: props.headingMargin }}>
+                <h2 className="heading-secondary">
+                    Most Popular Tours
+                </h2>
+        </div>
+    </ >
+)
+
+export default SectionTours
