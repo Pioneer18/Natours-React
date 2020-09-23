bd0a90051fff9497f86f395d9364ef6f3caff597
User <user@users-imac.home>
2020-09-23 16:50:48 -0400

    setting up a section-stories component

---
+++ src/Components/Main/SectionStories/SectionStories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SectionStories = props => {
+
+    return (
+        <>
+        <div className="u-center-text u-margin-bottom-large">
+            <h2 className="heading-secondary">
+                We make people genuinely happy
+            </h2>
+        </div>
+        </>
+    );
+}
+
+export default SectionStories;
