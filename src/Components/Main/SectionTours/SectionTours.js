1d6dca9d950a8260c5dc990ab8ba14b975d5c6e3
User <user@users-imac.home>
2020-09-23 12:08:45 -0400

    updated the cards list items as well as the backside of the card

--- src/Components/Main/SectionTours/SectionTours.js
+++ src/Components/Main/SectionTours/SectionTours.js
@@ -23,11 +23,23 @@ const SectionTours = props => (
                                 </span>
                             </h4>
                             <div className="card__details">
-                                Details
+                                <ul>
+                                    <li>3 day tours</li>
+                                    <li>Up to 30 people</li>
+                                    <li>2 tour guides</li>
+                                    <li>Sleep in cozy hotels</li>
+                                    <li>Difficulty: easy</li>
+                                </ul>
                             </div>
                         </div>
                         <div className='card__side card__side--back card__side--back-1'>
-                            Trunks
+                            <div className="card__cta">
+                                <div className="card__price-box">
+                                    <p className="card__price-only">Only</p>
+                                    <p className="card__price-value">$297</p>
+                                </div>
+                                <a href="#" className="btn btn--white">Book Now</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -43,11 +55,23 @@ const SectionTours = props => (
                                 </span>
                             </h4>
                             <div className="card__details">
-                                Details
+                                <ul>
+                                <li>7 day tours</li>
+                                <li>Up to 40 people</li>
+                                <li>6 tour guides</li>
+                                <li>Sleep in provided tents</li>
+                                <li>Difficulty: medium</li>
+                                </ul>
                             </div>
                         </div>
                         <div className='card__side card__side--back card__side--back-2'>
-                            World
+                            <div className="card__cta">
+                                <div className="card__price-box">
+                                    <p className="card__price-only">Only</p>
+                                    <p className="card__price-value">$297</p>
+                                </div>
+                                <a href="#" className="btn btn--white">Book Now</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -63,11 +87,23 @@ const SectionTours = props => (
                                 </span>
                             </h4>
                             <div className="card__details">
-                                Details
+                                <ul>
+                                <li>5 day tours</li>
+                                <li>Up to 15 people</li>
+                                <li>3 tour guides</li>
+                                <li>Sleep in provided tents</li>
+                                <li>Difficulty: hard</li>
+                                </ul>
                             </div>
                         </div>
                         <div className='card__side card__side--back card__side--back-3'>
-                            Pizza
+                            <div className="card__cta">
+                                <div className="card__price-box">
+                                    <p className="card__price-only">Only</p>
+                                    <p className="card__price-value">$297</p>
+                                </div>
+                                <a href="#" className="btn btn--white">Book Now</a>
+                            </div>
                         </div>
                     </div>
                 </div>
