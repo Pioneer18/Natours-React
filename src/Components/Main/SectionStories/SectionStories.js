4137f8e0192f0a16d6ba1cbf00c9f74f2a40d557
User <user@users-imac.home>
2020-09-24 01:09:21 -0400

    updated the stories section

--- src/Components/Main/SectionStories/SectionStories.js
+++ src/Components/Main/SectionStories/SectionStories.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import img_1 from '../../../Images/nat-8.jpg';
 
 const SectionStories = props => {
 
     return (
         <>
-            <div className="u-center-text u-margin-bottom-large">
-                <h2 className="heading-secondary">
-                    We make people genuinely happy
-                </h2>
-            </div>
+            <section className="section-stories">
+                <div className="u-center-text u-margin-bottom-large">
+                    <h2 className="heading-secondary">
+                        We make people genuinely happy
+                    </h2>
+                </div>
 
-            <div className="row">
-                <div className="story">TEXT</div>
-            </div>
+                <div className="row">
+                    <div className="story">
+                        <figure className="story__shape">
+                            <img src={img_1} alt="Person on a tour" className="story__img"/>
+                        </figure>
+                        <div className="story__text">
+                            <h3 className="heading__tertiary u-margin-bottom-small">I had the best week ever with my family</h3>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus deserunt doloremque alias dicta porro qui itaque laudantium necessitatibus consequatur aperiam, sunt voluptatum minima ducimus hic pariatur, eaque tenetur ea a?
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
