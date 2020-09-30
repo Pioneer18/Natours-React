f2e5a88efab0bc21e48bd179d65bf71b3f978f1e
User <user@users-imac.home>
2020-09-30 09:16:39 -0400

    updating the booking section

--- src/Components/Main/SectionBooking/SectionBooking.js
+++ src/Components/Main/SectionBooking/SectionBooking.js
@@ -8,7 +8,25 @@ const Booking = props => {
                 {/* Row to center the Booking Box */}
                 <div className="row">
                     <div className="book">
-                     
+                        <div className="book__form">
+                            <form action="#" className="form">
+                                <div className="u-margin-bottom-medium">
+                                    <h2 className="heading-secondary">
+                                        Start booking now
+                                    </h2>
+                                </div>
+                                <div className="form__group">
+                                    {/* Use htmlFor and id to link a label to it's input; when the label is clicked the input will be focused */}
+                                    <input type="text" className="form__input" placeholder="Full Name" id="name" required/>
+                                    <label htmlFor="name" className="form__label">Full name</label>
+                                </div>
+                                <div className="form__group">
+                                    {/* Remember 'email' is a type, the browser will check for valid email address format */}
+                                    <input type="email" className="form__input" placeholder="Email" id="email" required/>
+                                    <label htmlFor="email" className="form__label">Email</label>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </section>
