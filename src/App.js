125070e935f2af7f1d14bd5b4db489b04d6a8358
User <user@users-imac.home>
2020-09-30 20:44:29 -0400

    added a Footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Components/Header';
 import Main from './Components/Main';
+import Footer from './Components/Footer';
 import './sass/App.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Main headingMargin={'8rem'}></Main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from '../../Images/logo-green-2x.png'
+
+const Footer = props => {
+
+    return (
+        <footer className="footer">
+            <div className="footer__logo-box">
+                <img src={logo} alt="Full Logo" className="footer__logo"/>
+            </div>
+            <div className="row">
+                <div className="col-1-of-2">
+                    <div className="footer__navigation">
+                        <ul className="footer__list">
+                            <li className="footer__item"><a href="#" className="footer__link">Company</a></li>
+                            <li className="footer__item"><a href="#" className="footer__link">Careers</a></li>
+                            <li className="footer__item"><a href="#" className="footer__link">Contact Us</a></li>
+                            <li className="footer__item"><a href="#" className="footer__link">Privacy</a></li>
+                            <li className="footer__item"><a href="#" className="footer__link">Terms</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-1-of-2">
+                    <p className="footer__copyright">
+                        Built by <a href="#" className="footer__link">Jonathan D Sells</a> as a demonstrative webpage. Copyright &copy; by Jonathan D Sells, inspired directly by Jonas Schmedtmann
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
