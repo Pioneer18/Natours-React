398b6bc4d606d9d59d6df91a0cd0584df68653c2
User <user@users-imac.home>
2020-10-11 13:31:34 -0400

    updated the button for the title box

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../../sass/Components/_button.scss';
 
-const Button = props => (
-    <a href="#" className="btn btn--white btn--animated">Discover our tours</a>
-);
+
+{/* Button linked to id section-tours; css anchors */}
+const Button = props => {
+
+    return (
+        <a href="#section-tours" className="btn btn--white btn--animated" >Discover our tours</a>
+    );
+}
 
 export default Button;
